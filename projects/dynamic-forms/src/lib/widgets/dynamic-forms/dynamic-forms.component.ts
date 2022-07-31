e212e2d655a9733825b7421e8eb85fc3d3b69a17
Apurba Mondal <Apurba.Mondal@microfocus.com>
2022-08-01 05:00:13 +0530

    multi value uodate change.

--- projects/dynamic-forms/src/lib/widgets/dynamic-forms/dynamic-forms.component.ts
+++ projects/dynamic-forms/src/lib/widgets/dynamic-forms/dynamic-forms.component.ts
@@ -126,6 +126,7 @@ export class DynamicFormsComponent implements OnInit, AfterViewChecked {
       array.removeAt(item.i);
     });
     control.selection = [];
+    this.form.markAsDirty();
     this.emitChangeEvent(null);
   }
 
@@ -133,7 +134,7 @@ export class DynamicFormsComponent implements OnInit, AfterViewChecked {
   emitChangeEvent(event) {
     this.payLoad = JSON.stringify(this.form.getRawValue());
     this.form.valid ? this.isFormValid.emit(true) : this.isFormValid.emit(false);
-    this.form.valid && this.form.touched ? this.emitFormCurrentEvent() : false;
+    this.form.valid && this.form.dirty ? this.emitFormCurrentEvent() : false;
   }
 
   getFormConrtols(key) {
